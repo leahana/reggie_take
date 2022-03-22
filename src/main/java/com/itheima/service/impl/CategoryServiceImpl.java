fbc33c6efeba6dfd1280db3c0d24d0bb40b26b30
LeahAna <865578847@QQ.COM>
2022-03-22 11:52:30 +0800

    完成了数据库主从读写分离,我tm至少重装了四次虚拟机和环境,没想到居然是...配置文件..的..不知名空格..我真是纯纯铸币

--- src/main/java/com/itheima/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/itheima/service/impl/CategoryServiceImpl.java
@@ -12,10 +12,8 @@ import com.itheima.service.DishService;
 import com.itheima.service.SetmealService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
 public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category>
         implements CategoryService {
 
--- src/main/java/com/itheima/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/itheima/service/impl/EmployeeServiceImpl.java
@@ -1,15 +1,12 @@
 package com.itheima.service.impl;
 
-import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.itheima.entity.Employee;
 import com.itheima.mapper.EmployeeMapper;
 import com.itheima.service.EmployeeService;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
 public class EmployeeServiceImpl extends ServiceImpl<EmployeeMapper, Employee>
         implements EmployeeService {
 
